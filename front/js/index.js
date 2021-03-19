0b68dd3be1cc6ffecd1390d61e4f87c5d397812b
Axel Jouveaux <axl.j0uveaux@gmail.com>
2021-03-19 15:11:28 +0100

    mise en place du js pour la page d'accueil et recuperation de l'ID de l'article selectionne

--- front/js/index.js
+++ front/js/index.js
@@ -1,8 +1,8 @@
-
 fetch('http://localhost:3000/api/cameras').then(result => result.json()).then(cameras => {
 	let itemElt = document.getElementById('cameraPreview');
 	console.log(cameras);
 	cameras.forEach(camera => {
-		itemElt.innerHTML += `<div class="item"><img class="item__preview" src="${camera.imageUrl}"/><h2>${camera.name}</h2><p>${camera.price / 100}.00 €</div>`
+		itemElt.innerHTML += `<a class="item" href="./front/merch.html?id=${camera._id}"><div class="item__preview"><img class="item__preview--img" src="${camera.imageUrl}"/><div class="item__preview--desc"><h2 class="name">${camera.name}</h2><p class="price">${camera.price / 100}.00 €</p></div></div></a>`
 	})
 });
+
