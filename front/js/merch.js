3be0adb0d9c895ca8f752d2a81c37f4f55b316de
Axel Jouveaux <axl.j0uveaux@gmail.com>
2021-03-20 14:09:24 +0100

    correction du css de la page produit et debut du js pour ajouter au panier

--- front/js/merch.js
+++ front/js/merch.js
@@ -1,3 +1,18 @@
 let params = (new URL(document.location)).searchParams;
 let id = params.get('id');
-console.log(id);
+fetch('http://localhost:3000/api/cameras/'+id).then(result => result.json()).then(camera=> {
+	let imgElt = document.getElementById('img');
+	imgElt.innerHTML = `<img src="${camera.imageUrl}">`
+	let dataElt = document.getElementById('data');
+	dataElt.innerHTML = `<div class="product__description--title">
+							<p>${camera.name}</p>
+						</div>
+						<p>${camera.price / 100}</p>`
+	let descElt = document.getElementById('txt');
+	descElt.innerHTML = `<p>${camera.description}</p>`
+	let lensElt = document.getElementById('lens-select')
+	camera.lenses.forEach(lens => {
+		console.log(lens);
+		lensElt.innerHTML += `<option value="">${lens}</option>`
+	})
+})
